78217a745af6d67dad4af3342d91657bec8d1f75
YoungFrank <fluong95@gmail.com>
2018-10-17 13:57:31 -0400

    server links

--- server.js
+++ server.js
@@ -9,4 +9,18 @@ app.use(express.json());
 
 app.listen(PORT, function(){
     console.log('App listening on PORT: ' + PORT)
-})
+});
+
+var reservations = []; 
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "make.html"));
+});
+
+app.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "view.html"));
+});
