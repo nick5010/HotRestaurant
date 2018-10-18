208d13cf105646034b73c947aed7a7bd811645f6
nick5010 <nickgh5010@gmail.com>
2018-10-18 11:16:32 -0400

    js

--- server.js
+++ server.js
@@ -4,37 +4,45 @@ var path = require('path');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-app.listen(PORT, function(){
+app.listen(PORT, function () {
     console.log('App listening on PORT: ' + PORT)
 });
 
-var reservations = []; 
+var reservations = [];
 var waitlist = [];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.get('/api/tables', function(req, res){
+app.get('/api/tables', function (req, res) {
     return res.json(reservations)
 })
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "make.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/tables", function (req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
+
 });
 
-app.post('/api/tables', function(req, res){
+app.post('/api/tables', function (req, res) {
     var newPerson = req.body;
-    newPerson.routeName = newPerson.name.replace(/\s+/g, "").toLowerCase();
+    // newPerson.routeName = newPerson.name.replace(/\s+/g, "").toLowerCase();
     console.log(newPerson);
-    reservations.push(newPerson);
-    res.json(newPerson);
+    if (reservations.length < 6) {
+        reservations.push(newPerson);
+        res.json(newPerson);
+    } else {
+        waitlist.push(newPerson);
+        res.json(newPerson)
+    }
 
 })
